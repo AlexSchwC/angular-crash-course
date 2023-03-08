98ef9f6a30af29a6105f3410253e36b772b0b60e
Alexandre Schwanke da Costa <alexandreschwc@gmail.com>
2023-03-08 15:40:19 -0300

    Commit Inicial | Crash Course até os 51min

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,14 @@
+import { Task } from './../../Task';
+import { Component, Input } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.scss'],
+})
+export class TaskItemComponent {
+  @Input() task!: Task;
+
+  faTimes = faTimes;
+}
